1722f9649bf964d7272ee2fe2e6231d8908e4b9a
Mikhail Kalyujniy <mih.kalyujniy@yandex.ru>
2023-04-06 10:04:05 +0300

    123

---
+++ provision.sh
@@ -0,0 +1,10 @@
+useradd michael -m -p 3457275
+mkdir /home/michael/.ssh
+chmod 700 /home/michael/.ssh
+cat <<'EOT' > /home/michael/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDv+b8tqykFYqcfaK1wsFFvJuCpDABYK1OHeSFxIP9ePV2rlFb00L1b69ZQn0WbF8WMgTj2FNHk4mzOJBVRbkT8i76tORwSTY1y+05ttB8pbS9ues+EnSuXe0qYj43w8ziYpf1ZUWugI1FbQ9m2zeAWoK8yRgn9c51Vo9dJ+ae1Erj9a6ISH+EGXTTyvjE5u8JJap5mCeBMrUxzoo0FJWowBLML7qV3ww9Mdl/uhhRnjHyMtZ4rP82qf4ySCW8fv2pldurhXnTTz0GJn7Xw3AROq4mnp+0jv/9jJ9S7w7jenNiRzgYm6aSFvskkG3yk/vXGvo6rl1Glm6ZYLmNB/AIGeCKXEM0OmNIwUV2Ek305stO0n1g8lfbj7hFGzvNv+D8NQm90aSULn5dUx+af5bj935G0uaXOXZwXHi2bJ+l5pgtgmmlcZ2xxv7PwwVVAfwJhgma/QgVeIoc+RDU2IDIhoCU83WTXp/qg/sB/n3YxhvnRGszSkOF0A4GjrHTvt28= michael@hf-book
+
+EOT
+chmod 600 /home/michael/.ssh/authorized_keys
+chown michael:michael -R /home/michael/.ssh/
+yum install -y docker
---
+++ script.py
@@ -0,0 +1,18 @@
+import paramiko
+from scp import SCPClient
+
+ssh = paramiko.SSHClient()
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+
+ssh.connect(hostname='194.26.138.107', 
+            port = '443',
+            username='root')
+
+scp = SCPClient(ssh.get_transport())
+scp.put(f'.\\provision.sh', recursive=True, remote_path='/root')
+scp.close()
+ssh.exec_command('chmod +x /root/provision.sh')
+_stdin, _stdout,_stderr = ssh.exec_command('ls -la /root/provision.sh')
+print(_stdout.read().decode())
+ssh.close()
+input()
